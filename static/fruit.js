1601b74a1ed0b0f2666a35a99fd338e3a7d486ca
Erick Duarte <erickod@gmail.com>
2020-10-26 15:48:06 -0300

    Verification on update method

--- static/fruit.js
+++ static/fruit.js
@@ -5,21 +5,31 @@ class Fruit {
     constructor(){
     }
 
-    genarateRandomFruit(){
-        const fruitX = Math.floor(Math.random() * 20)
-        const fruitY = Math.floor(Math.random() * 20)
+    genarateRandomFruit(notification){
+        if(!notification) notification = {type:'getNewFruit'}
+        
+        if(notification.type == 'getNewFruit'){
+            const fruitX = Math.floor(Math.random() * 20)
+            const fruitY = Math.floor(Math.random() * 20)
 
-        const fruit = {
-            x: fruitY,
-            y: fruitX,
-            id: `${fruitX}-${fruitY}`
+            const fruit = {
+                x: fruitY,
+                y: fruitX,
+                id: `${fruitX}-${fruitY}`
+            }
+
+            this.fruits[fruit.id] = fruit
+            notification = fruitNotification()
+            notification.type = 'fruitGeneration'
+            notification.value = fruit
+            this.notifyAll(notification)
         }
-        this.fruits[fruit.id] = fruit
+    }
 
-        const notification = fruitNotification()
-        notification.type = 'fruitGeneration'
-        notification.value = fruit
-        this.notifyAll(notification)
+    deleteFruit(notification){
+        if(notification.type == 'deleteFruit'){
+            delete this.fruits[notification.value.id]
+        }
     }
 
     subscribe(subject){
@@ -31,14 +41,8 @@ class Fruit {
     }
 
     update(notification){
-        if(!notification){
-            return
-        }
-
-        if(notification.type == 'getNewFruit'){
-            this.genarateRandomFruit()
-        }
-        
+        this.genarateRandomFruit(notification)
+        this.deleteFruit(notification)
     }
 
     attach(observer){
