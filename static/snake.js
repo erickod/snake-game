112cf70c909a5de074913b377bbc8bf13fdfc222
Erick Duarte <erickod@gmail.com>
2020-10-26 12:36:12 -0300

    Refactor

--- static/snake.js
+++ static/snake.js
@@ -1,4 +1,7 @@
 class Snake {
+    observers = []
+
+    name = 'player1'
     position = {
         x:null,
         y:null
@@ -13,23 +16,110 @@ class Snake {
         },
     ]
 
-    subscrite(subject){
+    subscribe(subject){
         subject.attach(this)
     }
 
-    unsubscrite(subject){
+    unsubscribe(subject){
         subject.dettach(this)
     }
 
     update(notification){
        this.inputHandler(notification)
+       this.gameRefreshHandler(notification)
+    }
+
+    gameRefreshHandler(notification){
+        //console.log(notification)
+
+        // save head position
+        this.position.x = this.tail[0].x
+        this.position.y = this.tail[0].y
+
+        // Update position
+        if(this.direction == 'left'){
+            if(this.position.x <= 0 ){
+                this.position.x = 19
+            }else {
+                this.position.x -= 1
+            }
+        }
+
+        if(this.direction == 'right'){
+            if(this.position.x >= 19 ){
+                this.position.x = 0
+            }else {
+                this.position.x += 1
+            }
+        }
+        
+        if(this.direction == 'up'){
+            if(this.position.y <= 0 ){
+                this.position.y = 19
+            }else {
+                this.position.y -= 1
+            }
+        }
+
+        if(this.direction == 'down'){
+            if(this.position.y >= 19 ){
+                this.position.y = 0
+            }else {
+                this.position.y += 1
+            }
+        }
+        
+        //update snake tail  
+        this.tail.pop()
+        this.tail.unshift({x:this.position.x, y:this.position.y})
+
+        //notify position
+        const notificationSnakePosition = snakeNotification()
+        notificationSnakePosition.type = 'snakePosition'
+        
+        notificationSnakePosition.value = {
+            player: this.name, 
+            x:this.position.x, 
+            y:this.position.y,
+            tail: this.tail}
+
+        
+
+        this.notifyAll(notificationSnakePosition)
     }
 
     inputHandler(notification){
-        if(notification.type != 'input'){
+        if(!notification || notification.type != 'input'){
             return
         }
         this.direction = notification.value
+    }
+
+    attach(observer){
+        if(!(observer in this.observers)){
+            this.observers.push(observer)
+        }
+    }
+
+    dettach(){
+        const i = this.observers.indexOf(observer)
+        if(i){
+            delete this.observers[i]
+        }
+    }
+
+    notifyAll(notification){
+        for(let observer of this.observers){
+            observer.update(notification)
+        }
+    }
+}
+
+const snakeNotification = () => {
+    return {
+        sender: 'Snake',
+        type:'',
+        value:''
     }
 }
 
