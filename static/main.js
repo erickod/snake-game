1e1b70eb915f54383da9c87dc7ab41ebb6ba2dca
Erick Duarte <erickod@gmail.com>
2020-10-27 19:35:10 -0300

    Right order

--- static/main.js
+++ static/main.js
@@ -6,9 +6,10 @@ import { NetworkHandler } from './network.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('main.js loaded');
+    const socket = io()
 
     const inputHandler = new InputHandler()
-    const netHandler = new NetworkHandler()
+    const netHandler = new NetworkHandler(socket)
     const localPlayer = new Snake()
     const board = new Board()
     window.board = board
@@ -28,10 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
     let count = 0
 
     let boardRefresh = setInterval(() => {
+        board.gameRefresh()
         board.drawBoard()
         board.drawSnake()
         board.drawFuit()
-        board.gameRefresh()
         board.hasSnakeTheSamePositionWichFruit()
 
         count ++
